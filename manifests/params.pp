16c6fabd7e23ff27530d2719a7f5ff75c93c05f2
Tom Murphy <tdm4@users.noreply.github.com>
2018-07-18 11:08:45 +0100

    Set JDK_DIRS properly depending on SOLR version. Remove precise as that is no longer used.

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,23 +15,45 @@ class solr::params {
   $dist_root     = '/tmp'
   $heap_size     = "256m"
 
-  case $::lsbdistcodename {
-    'precise': {
-      $jetty_home    = '/usr/share/jetty'
-      $jetty_package = 'jetty'
-      $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-6-sun'
-    }
+  if versioncmp($::solr::version, '4.0') < 0 {
+    case $::lsbdistcodename {
+      'trusty': {
+        $jetty_home    = '/usr/share/jetty'
+        $jetty_package = 'jetty'
+        $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
+      }
 
-    'trusty': {
-      $jetty_home    = '/usr/share/jetty'
-      $jetty_package = 'jetty'
-      $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
-    }
+      'xenial': {
+        $jetty_home    = '/usr/share/jetty'
+        $jetty_package = 'jetty'
+        $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
+      }
 
-    default: {
-      $jetty_home    = '/usr/share/jetty8'
-      $jetty_package = 'jetty8'
-      $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
+      default: {
+        $jetty_home    = '/usr/share/jetty8'
+        $jetty_package = 'jetty8'
+        $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
+      }
+    }
+  } else {
+    case $::lsbdistcodename {
+      'trusty': {
+        $jetty_home    = '/usr/share/jetty'
+        $jetty_package = 'jetty'
+        $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-8-openjdk-amd64'
+      }
+
+      'xenial': {
+        $jetty_home    = '/usr/share/jetty'
+        $jetty_package = 'jetty'
+        $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-8-openjdk-amd64'
+      }
+
+      default: {
+        $jetty_home    = '/usr/share/jetty8'
+        $jetty_package = 'jetty8'
+        $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-8-openjdk-amd64'
+      }
     }
   }
 }
