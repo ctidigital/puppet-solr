ac7681c80018def908cbe95d27073bc04f05c4df
tdm4 <tdm4@users.noreply.github.com>
2019-01-28 14:47:29 +0000

    Improvement/move to open jdk 8 from oracle java (#10)
    * Remove Oracle Java 8 dependency and use OpenJDK 8 instead.
    
    * Clean up after Oracle Java 8
    
    * Ensure Jetty does not get installed if SOLR version is 5.0 or greater as SOLR is self-contained from then on

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,50 +10,71 @@ class solr::install {
 
   ensure_packages(['python-software-properties', 'software-properties-common'])
 
-  if versioncmp($::solr::version, '5.0') < 0 {
-    case $::lsbdistcodename {
+  if ! defined(Package['oracle-java8-set-default']) {
+    package { 'oracle-java8-set-default':
+      ensure => purged,
+      before => Exec['Remove_Oracle_Java_PPA'],
+    }
+  }
 
-      'trusty': {
-        if ! defined(Package['default-jdk']) {
-          package { 'default-jdk':
-          ensure    => present,
-          }
-        }
+  if ! defined(Package['oracle-java8-installer']) {
+    package { 'oracle-java8-installer':
+      ensure => purged,
+      before => Exec['Remove_Oracle_Java_PPA'],
+    }
+  }
 
-        if ! defined(Package['jetty']) {
-          package { 'jetty':
-            ensure  => present,
-            require => Package['default-jdk'],
-          }
-        }
+  exec { 'Remove_Oracle_Java_PPA':
+    path    => [ '/bin', '/sbin' , '/usr/bin', '/usr/sbin', '/usr/local/bin' ],
+    command => 'add-apt-repository -y --remove ppa:webupd8team/java; apt-get -y update',
+    onlyif  => "test -s /etc/apt/sources.list.d/webupd8team-ubuntu-java-$::lsbdistcodename.list",
+  }
 
-        if ! defined(Package['libjetty-extra']) {
-          package { 'libjetty-extra':
-            ensure  => present,
-            require => Package['jetty'],
-          }
+  case $::lsbdistcodename {
+
+    'trusty': {
+      if ! defined(Package['default-jdk']) {
+        package { 'default-jdk':
+        ensure    => present,
         }
       }
 
-      'xenial': {
-        exec { 'Add_OpenJDK_Repo':
-          path    => [ '/bin', '/sbin' , '/usr/bin', '/usr/sbin', '/usr/local/bin' ],
-          command => 'add-apt-repository -y ppa:openjdk-r/ppa; apt-get -y update',
-          creates => '/etc/apt/sources.list.d/openjdk-r-ubuntu-ppa-xenial.list',
+      if ! defined(Package['jetty']) {
+        package { 'jetty':
+          ensure  => present,
+          require => Package['default-jdk'],
         }
+      }
 
-        if versioncmp($::solr::version, '4.0') >= 0 {
-          $java_package="openjdk-8-jdk-headless"
-        } else {
-          $java_package="openjdk-7-jre-headless"
+      if ! defined(Package['libjetty-extra']) {
+        package { 'libjetty-extra':
+          ensure  => present,
+          require => Package['jetty'],
         }
+      }
+    }
 
-        if ! defined(Package["${java_package}"]) {
-          package { $java_package:
-            ensure  => present,
-            require => Exec['Add_OpenJDK_Repo'],
-          }
+    'xenial': {
+      exec { 'Add_OpenJDK_Repo':
+        path    => [ '/bin', '/sbin' , '/usr/bin', '/usr/sbin', '/usr/local/bin' ],
+        command => 'add-apt-repository -y ppa:openjdk-r/ppa; apt-get -y update',
+        creates => '/etc/apt/sources.list.d/openjdk-r-ubuntu-ppa-xenial.list',
+      }
+
+      if versioncmp($::solr::version, '4.0') >= 0 {
+        $java_package="openjdk-8-jdk-headless"
+      } else {
+        $java_package="openjdk-7-jre-headless"
+      }
+
+      if ! defined(Package["${java_package}"]) {
+        package { $java_package:
+          ensure  => present,
+          require => Exec['Add_OpenJDK_Repo'],
         }
+      }
+
+      if versioncmp($::solr::version, '5.0') < 0 {
 
         if ! defined(Package['jetty8']) {
           package { 'jetty8':
@@ -79,19 +100,9 @@ class solr::install {
           require => Package['jetty8'],
         }
       }
-      default: { }
     }
-  } else {
-    exec { 'install-java8-for-solr':
-      path    => [ '/bin', '/sbin' , '/usr/bin', '/usr/sbin', '/usr/local/bin' ],
-      command => 'add-apt-repository -y ppa:webupd8team/java;\
-apt-get -y update;\
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true |\
-/usr/bin/debconf-set-selections; apt-get -y install oracle-java8-installer',
-      require => Package['python-software-properties', 'software-properties-common'],
-      timeout => 900,
-      creates => '/usr/lib/jvm/java-8-oracle'
-    }
+
+    default: { }
   }
 
   if ! defined(Package['wget']) {
